067025c5fbe4cc40cfa6e42207ae9127e73dd655
anmol <you135794@example.com>
2022-12-22 12:09:29 +0530

    Stock

--- lib/Screens/Details.dart
+++ lib/Screens/Details.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 
 class Details extends StatefulWidget {
   String name,image;
@@ -10,7 +10,8 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
-  late String quantity,date,name,image;
+  late String quantity,name,image;
+  var date=(DateFormat.yMMMEd().format(DateTime.now())).toString();
 
   _DetailsState(this.name,this.image);
   @override
@@ -21,17 +22,46 @@ class _DetailsState extends State<Details> {
         title: Text('Buy Stock'),
       ),
       backgroundColor: Colors.orange,
-      body: Card(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.all(50.0)),
-              Image(image: AssetImage(image),width: 100.0,height: 100.0,),
-              Text(name),
-
-            ],
+      body: Container(
+        width: 500.0,
+        child: Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ListTile(
+                      leading: Icon(Icons.calendar_month),
+                      title: Text(date,style: TextStyle(color: Colors.black),),
+                  ),
+                  Padding(padding: EdgeInsets.fromLTRB(50.0,20.0,50.0,5.0)),
+                  Image(image: AssetImage(image),width: 200.0,height: 200.0,),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Text('Name: $name',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    
+                  ),
+                  Padding(padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Quantity: ',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold)),
+                      
+                    ],),),
+                    Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: ElevatedButton(onPressed: (){}, child: Text('Confirm Purchase',style: TextStyle(fontSize: 20.0),)),
+                    ),
+                ],
+              ),
+            
           ),
-        ), 
+      ), 
         
     );
   }
